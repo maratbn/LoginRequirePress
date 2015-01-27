acb6031e46b991cb9e8b8ed96dc28894caeecc78
Marat Nepomnyashy <maratbn@gmail.com>
2015-01-26 20:18:23 -0800

    Added 'PasswordProtectPress.php'.

---
+++ PasswordProtectPress.php
@@ -0,0 +1,43 @@
+<?php
+/*
+  Plugin Name: PasswordProtectPress
+  Plugin URI: http://www.maratbn.com/
+  Description: WordPress plugin for password-protecting regular posts and pages on a WordPress
+               site, making them accessible to regular site subscribers upon login.
+  Author: Marat Nepomnyashy
+  Author URI: http://www.maratbn.com
+  License: GPL3
+  Version: 0.0.1-development_unreleased
+*/
+
+/*
+  PasswordProtectPress -- WordPress plugin for password-protecting regular
+                          posts and pages on a WordPress site, making them
+                          accessible to regular site subscribers upon login.
+
+  Copyright (C) 2015  Marat Nepomnyashy  http://maratbn.com  maratbn@gmail
+
+  Version:        0.0.1-development_unreleased
+
+  Module:         PasswordProtectPress.php
+
+  Description:    Main PHP file for the WordPress plugin 'PasswordProtectPress'.
+
+  This file is part of PasswordProtectPress.
+
+  Licensed under the GNU General Public License Version 3.
+
+  PasswordProtectPress is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  PasswordProtectPress is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with PasswordProtectPress.  If not, see <http://www.gnu.org/licenses/>.
+*/
+?>
