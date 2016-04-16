5be93b0830280499bf0db19c669e5b3f94ee27a3
Marat Nepomnyashy <maratbn@gmail.com>
2016-04-15 19:55:53 -0700

    Whitespace adjustment.

--- loginrequirepress/LoginRequirePress.php
+++ loginrequirepress/LoginRequirePress.php
@@ -278,7 +278,9 @@
                     <td><a href='<?=\get_edit_post_link($idPost)?>'><?=$strPostName?></a></td>
                     <td><?=$post->post_type?></td>
                     <td><?=\get_page_template_slug($idPost)?></td>
-                    <td><?=$strPostStatus?></td>
+                    <td>
+                      <?=$strPostStatus?>
+                    </td>
                   </tr><?php
                     $indexRow++;
                 }
