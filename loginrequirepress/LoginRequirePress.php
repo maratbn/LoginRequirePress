a25564261bf8350d6f51ba68427dcd0f3d1e2e15
Marat Nepomnyashy <maratbn@gmail.com>
2016-06-12 17:15:15 -0700

    Whitespace adjustment.

--- loginrequirepress/LoginRequirePress.php
+++ loginrequirepress/LoginRequirePress.php
@@ -275,8 +275,8 @@
                   'domain-plugin-LoginRequirePress'));
                    ?></p><?php
             ?><p><?=\sprintf(
-              \__('After submitting the form, make sure that any post(s) you want login-protected ' .
-                  'are listed in the \'%1$s\' section below.',
+              \__('After submitting the form, make sure that any post(s) you want ' .
+                  'login-protected are listed in the \'%1$s\' section below.',
                   'domain-plugin-LoginRequirePress'),
               \__('Non-private login-protected post(s)',
                   'domain-plugin-LoginRequirePress'))
